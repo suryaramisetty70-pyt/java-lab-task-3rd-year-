913601f7eed9d5797b5d33cd89f7441a9fc18b64
Suryaramisetty <surya@example.com>
2026-08-05 15:13:15 +0530

    Week 3 Session 15: Sort Array By Parity using Custom Comparator

---
+++ Week3/Week3_Session15_SortArrayByParity.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Week3_Session15_SortArrayByParity {
+    public static int[] sortArrayByParity(int[] nums) {
+        Integer[] boxed = Arrays.stream(nums).boxed().toArray(Integer[]::new);
+        Arrays.sort(boxed, (a, b) -> Integer.compare(a % 2, b % 2));
+        return Arrays.stream(boxed).mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 1, 2, 4};
+        int[] sorted = sortArrayByParity(nums);
+
+        System.out.println("Original Array: " + Arrays.toString(nums));
+        System.out.println("Sorted by Parity: " + Arrays.toString(sorted));
+    }
+}
+
+/*
+OUTPUT:
+Original Array: [3, 1, 2, 4]
+Sorted by Parity: [2, 4, 3, 1]
+*/
